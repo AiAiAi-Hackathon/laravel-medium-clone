2c52b0e33ca5e361fcbd0fcf07a3573bdc73e822
Juan Diego Carballo <juandiegocarballo2701@gmail.com>
2025-05-17 14:39:35 -0600

    refactor: Update ingestion logic to process multiple file types dynamically

--- ingest.py
+++ ingest.py
@@ -8,6 +8,16 @@ LLAMA_INDEX_API_KEY = os.getenv("LLAMA_INDEX_API_KEY")
 parser = LlamaParse(
     api_key = LLAMA_INDEX_API_KEY,
 )
-
-result = parser.parse("README.md.txt")   
-print(result)
+allowed_extensions = [
+    "txt",
+    "json",
+    "xml",
+]
+for root, dirs, files in os.walk("."):
+    for filename in files:
+        ext = filename.split(".")[-1]
+        if ext in allowed_extensions:
+            with open(os.path.join(root, filename), "r") as f:
+                content = f.read()
+                result = parser.parse(content)
+                print(result)
