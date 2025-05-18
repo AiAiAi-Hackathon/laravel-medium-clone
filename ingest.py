bb61ea406c2c3a56d2dfe7fdb3472b842116ea34
JeffP <jeffrey.perez@gorillalogic.com>
2025-05-17 18:23:10 -0600

    Rename code files before sending to Llama

--- ingest.py
+++ ingest.py
@@ -3,12 +3,18 @@ from llama_index.core.readers import SimpleDirectoryReader
 from llama_cloud_services import LlamaParse
 
 def parse(file_path):
-    data = LlamaParse(result_type="json").load_data(file_path)
+    try:
+        data = LlamaParse(result_type="json").load_data(file_path)
+    except Exception as e:
+        print(f"Error parsing {file_path}: {e}")
+        data = []
     return data
-    
+
 all_dirs = ["."]
 
 excluded_dirs = [".pylibs", ".git"]
+
+included_extensions = [".md", ".php", ".js"]
 
 def get_all_dirs(path):
     print(f".......Scanning directory: {path}")
@@ -23,6 +29,12 @@ def get_all_dirs(path):
 get_all_dirs("./")
 
 for d in all_dirs:
+    for entry in os.listdir(d):
+        if entry.endswith(tuple(included_extensions)):
+            full_path = os.path.join(d, entry)
+            new_full_path = full_path + ".txt"
+            os.rename(full_path, new_full_path)
+            print(f"Renamed {full_path} to {new_full_path}")
     try:
         #documents = SimpleDirectoryReader(d).load_data()
         documents = parse(d)
